cfa6669cdcd0c532428a3727d1eba0b398b9743b
Wang, Eikan <eikan.wang@intel.com>
2024-04-22 14:43:46 +0000

    Add registration API for torch.compile-eager
    ghstack-source-id: 7ff364902962897b247d2de56aede334100678aa
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/121387

--- torch/csrc/utils/python_dispatch.h
+++ torch/csrc/utils/python_dispatch.h
@@ -1,10 +1,13 @@
+#pragma once
+
 #include <pybind11/pybind11.h>
 #include <torch/csrc/utils/pybind.h>
 
-namespace torch {
-namespace impl {
-namespace dispatch {
+#include <ATen/ATen.h>
+#include <ATen/core/boxing/KernelFunction.h>
+#include <c10/core/SafePyObject.h>
 
+namespace torch::impl::dispatch {
 void initDispatchBindings(PyObject* module);
 
 void python_op_registration_trampoline_impl(
@@ -14,6 +17,21 @@ void python_op_registration_trampoline_impl(
     torch::jit::Stack* stack,
     bool with_keyset);
 
-} // namespace dispatch
-} // namespace impl
-} // namespace torch
+class PythonKernelHolder : public c10::OperatorKernel {
+  c10::SafePyObject func_;
+  c10::DispatchKey dispatch_key_;
+  // If "with_keyset", then we expect a keyset as the first arg.
+  bool with_keyset_;
+
+ public:
+  PythonKernelHolder(
+      py::object func,
+      c10::DispatchKey dispatch_key,
+      bool with_keyset = false);
+  void operator()(
+      const c10::OperatorHandle& op,
+      c10::DispatchKeySet keyset,
+      torch::jit::Stack* stack);
+};
+
+} // namespace torch::impl::dispatch
